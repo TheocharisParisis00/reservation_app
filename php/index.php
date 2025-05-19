1b871d527cfaf65bd682d1cd7e07a21b5751d4de
θεοχαρης παρισης <parisisxaris@gmail.com>
2025-05-19 14:12:17 +0200

    .

--- php/index.php
+++ php/index.php
@@ -2,5 +2,20 @@
 
 require_once 'database/conn.php';
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Main Page</title>
+</head>
+<body>
+    
+</body>
+<script src="js/script.js"></script>
+</html>
+<?php
 
 ?>
